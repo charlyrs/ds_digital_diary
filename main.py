3371a0aeee039b7ecec2ce9f24a1d2dd3789bef0
Liliya <charlyrsmail@gmail.com>
2023-12-09 21:22:52 +0300

    update

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ import streamlit as st
 import pandas as pd
 
 
+class user_class:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+
+    def __str__(self):
+        return self.name
+
+def make_unique(strings):
+    d = {}
+    for s in strings:
+        d[s] = d.get(s, 0) + 1
+    res = []
+    for s in strings:
+        if d[s] == 1:
+            res.append(s)
+        else:
+            res.append(s+"_"+str(d[s]-1))
+            d[s] = d[s] - 1
+    return res
+
 st.set_page_config(layout="wide")
 values = dotenv_values()
 name = values["NAME"]
@@ -22,24 +43,73 @@ tables = pd.read_sql_query("""SELECT table_name
    AND table_type='BASE TABLE';
 """, con=engine)
 tables = [t for t in tables["table_name"].tolist() if t != "temp_table"]
-table = st.selectbox("Select table", options=tables)
+table = st.selectbox("Select table", options=tables[::-1])
+users = pd.read_sql_query(f"select * from users", con=engine)
+# entries = pd.read_sql_query(f"select * from diaryentries", con=engine)
 df = pd.read_sql_query(f"select * from {table}", con=engine)
 df = df.set_index(df.columns[0], drop=True)
 pddict = {}
 for col, dtype in df.dtypes.items():
     pddict[col] = pd.Series([], dtype=dtype)
 add_df = pd.DataFrame(pddict)
-edited = st.data_editor(
-    df[~df["delete"]],
-    hide_index=True,
-    column_config={
-        "moodrating": st.column_config.SelectboxColumn(
-            "Mood rating",
-            options=list(range(11)),
-            required=True,
-        ),
-    },
-)
+mapper = {id_user: name for id_user, name in zip(users["userid"].tolist(),users["name"].tolist())}
+#names = make_unique(list(mapper.values()))
+if table == "diaryentries":
+    temp_df = df[~df["delete"]].copy()
+    temp_df["change_user"] = False
+    temp_df["user_name"] = [mapper[x["userid"]] for ind, x in temp_df.iterrows()]
+    edited = st.data_editor(
+        temp_df,
+        hide_index=True,
+        column_config={
+            "moodrating": st.column_config.SelectboxColumn(
+                "Mood rating",
+                options=list(range(11)),
+                required=True,
+            ),
+            "userid": None,
+            "user_name" : st.column_config.Column(disabled=True)
+        },
+    )
+else:
+    edited = st.data_editor(
+        df[~df["delete"]].copy(),
+        hide_index=True,
+        column_config={
+            "moodrating": st.column_config.SelectboxColumn(
+                "Mood rating",
+                options=list(range(11)),
+                required=True,
+            ),
+        },
+    )
+
+if st.button("Edit data"):
+    id_col = df.index.name
+    with engine.begin() as conn:
+        if table != "users": edited = edited.drop(["change_user", "user_name"], axis=1)
+        edited_rows = edited[(df[~df["delete"]] != edited).sum(axis=1) > 0]
+        if len(edited_rows) > 0:
+            print("EDIT")
+            edited_rows.to_sql('temp_table', engine, if_exists='replace')
+            print(edited_rows)
+            for col in edited_rows.columns:
+                sql = f"UPDATE {table} AS f SET {col} = t.{col} FROM temp_table AS t WHERE f.{id_col} = t.{id_col}"
+                conn.execute(text(sql))
+else:
+    if table == "diaryentries":
+        users = pd.read_sql_query(f"select * from users", con=engine)
+        users = [user_class(x["name"], x["userid"]) for ind, x in users.iterrows()]
+        change_df = edited[edited["change_user"]]
+        with st.form("form"):
+            st.write("Edit user for chosen entry")
+            label_select = "No entry selected" if len(change_df) == 0 else change_df.iloc[0, :]["textcontent"]
+            new_user = st.selectbox(options=users, label=label_select)
+            b = st.form_submit_button("change")
+            if b:
+                with engine.begin() as conn:
+                    sql = f"UPDATE {table} AS f SET userid = {new_user.id} WHERE entryid = {change_df.index[0]}"
+                    conn.execute(text(sql))
 added = st.data_editor(
     add_df,
     hide_index=True,
@@ -51,25 +121,17 @@ added = st.data_editor(
             required=True,
         ),
         "delete": None,
-
     },
 )
-if st.button("Change data"):
-    id_col=df.columns[0]
+if st.button("Add data"):
     last_ind = max(df.index) + 1
     added.index = list(range(last_ind, last_ind + len(added)))
     added["delete"] = False
-    with engine.begin() as conn:
-        edited_rows = edited[(df != edited).sum(axis=1) > 0]
-        if len(edited_rows) > 0:
-            edited_rows.to_sql('temp_table', engine, if_exists='replace')
-            for col in edited_rows.columns:
-                sql = f"UPDATE {table} AS f SET {col} = t.{col} FROM temp_table AS t WHERE f.{id_col} = t.{id_col}"
-                conn.execute(text(sql))
-        if len(added) > 0:
+    if len(added) > 0:
+        with engine.begin() as conn:
             added.to_sql('temp_table', engine, if_exists='replace')
             sql = f"INSERT INTO {table} (SELECT * FROM temp_table)"
             conn.execute(text(sql))
+            add_df = None
     st.write("Done")
-
 
